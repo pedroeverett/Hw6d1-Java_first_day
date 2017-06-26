5febd9c9aaff4e038b5195698284f592e68d03a7
Pedro Everett <pedrojoseeverett@gmail.com>
2017-06-26 19:19:41 +0100

    passengerCount test ok

--- Bus.java
+++ Bus.java
@@ -1,13 +1,27 @@
 public class Bus {
 
   private String name;
+  private Person[] passenger;
 
   public Bus(String name) {
     this.name = name;
+    this.passenger = new Person[20];
   }
 
   public String getName() {
     return this.name;
+  }
+
+  public int passengerCount() {
+    int numberOfPassangers = 0;
+
+    for (Person passenger : this.passenger) {
+      if (passenger != null) {
+        numberOfPassangers++;
+      }
+    }
+
+    return numberOfPassangers;
   }
 
 
--- BusTest.java
+++ BusTest.java
@@ -5,15 +5,22 @@ import org.junit.Test;
 public class BusTest {
 
   Bus bus;
+  Person passenger;
 
   @Before
   public void before() {
     bus = new Bus("Bus 26");
+    passenger = new Person();
   }
 
   @Test
   public void checkName() {
     assertEquals("Bus 26", bus.getName());
+  }
+
+  @Test
+  public void noPassangers() {
+    assertEquals(0, bus.passengerCount());
   }
 
 }
